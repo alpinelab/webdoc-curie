e6ad3b761cf5c99b81cbae653c541da6e19efabc
thibaultdalban <thibault.dalban@alpine-lab.com>
2014-10-28 19:26:31 +0100

    Implement POC search feature

--- source/javascripts/vendor/tipuesearch/_tipuedrop.js
+++ source/javascripts/vendor/tipuesearch/_tipuedrop.js
@@ -13,7 +13,7 @@ http://www.tipue.com/drop
 
           var set = $.extend( {
 
-               'show'                   : 3,
+               'show'                   : 10,
                'speed'                  : 300,
                'newWindow'              : false,
                'mode'                   : 'static',
@@ -51,38 +51,52 @@ http://www.tipue.com/drop
 
                function getTipuedrop($obj)
                {
-                    if ($obj.val())
+                  if ($obj.val())
+                  {
+                     var c = 0;
+                    var results = {};
+                    results["mastectomy"] = [];
+                    results["surgical"] = [];
+                    results["nonsurgical"] = [];
+                     for (var i = 0; i < tipuedrop_in.pages.length; i++)
+                     {
+                        var pat = new RegExp($obj.val(), 'i');
+                        if ((tipuedrop_in.pages[i].title.search(pat) != -1 || tipuedrop_in.pages[i].text.search(pat) != -1 || tipuedrop_in.pages[i].tags.search(pat) != -1) && c < set.show)
+                        {
+                          results[tipuedrop_in.pages[i].category].push('<a href="' + tipuedrop_in.pages[i].loc + '"><div class="tipue_drop_item"><div class="tipue_drop_left"><img src="' + tipuedrop_in.pages[i].thumb + '" class="tipue_drop_image"></div><div class="tipue_drop_right"><div class="tipue_drop_right_title">' + tipuedrop_in.pages[i].title + '</div><div class="tipue_drop_right_text">' + tipuedrop_in.pages[i].text + '</div></div></div></a>');
+                          c++;
+                        }
+                     }
+                     if (c != 0)
+                     {
+                        var elt = '<div id="tipue_drop_wrapper"><div class="tipue_drop_head"><div id="tipue_drop_head_text">RÉSULTATS DE VOTRE RECHERCHE</div></div>';
+                        elt += render_category("mastectomy", results["mastectomy"]);
+                        elt += render_category("surgical", results["surgical"]);
+                        elt += render_category("nonsurgical", results["nonsurgical"]);
+                        elt += '</div>';
+                        $('#tipue_drop_content').html(elt);
+                        $('#tipue_drop_content').fadeIn(set.speed);
+                     }
+                  }
+                  else
+                  {
+                       $('#tipue_drop_content').fadeOut(set.speed);
+                  }
+               }
+
+               function render_category(category, results)
+               {
+                  var elt = '<div id="' + category + '" class="search-' + category + '">';
+                  for (var i = 0; i < results.length; i++)
+                  {
+                    if (i == 0)
                     {
-                         var c = 0;
-                         for (var i = 0; i < tipuedrop_in.pages.length; i++)
-                         {
-                              var pat = new RegExp($obj.val(), 'i');
-                              if ((tipuedrop_in.pages[i].title.search(pat) != -1 || tipuedrop_in.pages[i].text.search(pat) != -1 || tipuedrop_in.pages[i].tags.search(pat) != -1) && c < set.show)
-                              {
-                                   if (c == 0)
-                                   {
-                                        var out = '<div id="tipue_drop_wrapper"><div class="tipue_drop_head"><div id="tipue_drop_head_text">Suggested results</div></div>';
-                                   }
-                                   out += '<a href="' + tipuedrop_in.pages[i].loc + '"';
-                                   if (set.newWindow)
-                                   {
-                                        out += ' target="_blank"';
-                                   }
-                                   out += '><div class="tipue_drop_item"><div class="tipue_drop_left"><img src="' + tipuedrop_in.pages[i].thumb + '" class="tipue_drop_image"></div><div class="tipue_drop_right"><div class="tipue_drop_right_title">' + tipuedrop_in.pages[i].title + '</div><div class="tipue_drop_right_text">' + tipuedrop_in.pages[i].text + '</div></div></div></a>';
-                                   c++;
-                              }
-                         }
-                         if (c != 0)
-                         {
-                              out += '</div>';
-                              $('#tipue_drop_content').html(out);
-                              $('#tipue_drop_content').fadeIn(set.speed);
-                         }
+                      elt += '<div class="category-title title-'+ category +'">' + category + '</div>';
                     }
-                    else
-                    {
-                         $('#tipue_drop_content').fadeOut(set.speed);
-                    }
+                    elt += results[i];
+                  }
+                  elt += '</div>';
+                  return elt;
                }
 
                $('html').click(function()
