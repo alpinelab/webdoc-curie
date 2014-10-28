7f7a2d2c3e59b517460cad3355045aa01db9f77a
thibaultdalban <thibault.dalban@alpine-lab.com>
2014-10-28 14:00:26 +0100

    Implement video intro

--- config.rb
+++ config.rb
@@ -24,6 +24,11 @@ with_layout :landing do
   page "/pages/landing/*"
 end
 
+with_layout :fullscreen do
+  page "/pages/mastectomy"
+end
+
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
