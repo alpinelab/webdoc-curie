3ab7ea3c44f884c34c77b309e0bc1119c38cf26c
thibaultdalban <thibault.dalban@alpine-lab.com>
2014-11-03 22:55:18 +0100

    Show an image when the video is loading

---
+++ source/javascripts/vendor/_bigvideo-image.js
@@ -0,0 +1,36 @@
+$(function() {
+  var $bigImage = $('.big-image'), $window = $(window);
+  $.BV.getPlayer().on('loadeddata', function() {
+    onVideoLoaded();
+  });
+  $bigImage.imagesLoaded(adjustImagePositioning);
+  $window.on('resize', adjustImagePositioning);
+  function onVideoLoaded() {
+    $('.big-image').transit({'opacity':0},500);
+  }
+  function adjustImagePositioning() {
+    var img = new Image();
+    img.src = $bigImage.attr('src');
+    var windowWidth = $window.width(),
+      windowHeight = $window.height(),
+      r_w = windowHeight / windowWidth,
+      i_w = img.width,
+      i_h = img.height,
+      r_i = i_h / i_w,
+      new_w, new_h, new_left, new_top;
+    if( r_w > r_i ) {
+      new_h   = windowHeight;
+      new_w   = windowHeight / r_i;
+    }
+    else {
+      new_h   = windowWidth * r_i;
+      new_w   = windowWidth;
+    }
+    $bigImage.css({
+      width   : new_w,
+      height  : new_h,
+      left    : ( windowWidth - new_w ) / 2,
+      top     : ( windowHeight - new_h ) / 2
+    })
+  }
+});
